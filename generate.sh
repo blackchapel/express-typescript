c523d47e1236ea5b1f7ee7b2f693c2122bf0cac7
blackchapel <kc.dark@protonmail.com>
2023-02-24 12:50:38 +0530

    MVC arch files generation script

---
+++ generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ask the user for file name
+read -p 'file name: ' filevar
+
+# Create the files with ref to MVC architecture
+touch ./src/controllers/$filevar.controller.ts
+touch ./src/services/$filevar.service.ts
+touch ./src/routes/$filevar.route.ts
+touch ./src/models/$filevar.model.ts
